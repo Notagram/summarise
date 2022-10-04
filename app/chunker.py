8f81f46c613d37b17ad0762a18ec39b7a6ccb656
Kyber <abdurraqeeb53@gmail.com>
2022-10-04 05:19:23 +0000

    CHANGES

--- app/chunker.py
+++ app/chunker.py
@@ -1,10 +1,10 @@
 import re
 from nltk import tokenize
 
-with open("transcript.txt") as f:
+with open("transcript.txt") as f: 
     text = f.read()
     
-sentences = tokenize.sent_tokenize(text)
+sentences = tokenize.sent_tokenize(text) 
 
 chunks = []
 max_length = 13000
@@ -13,13 +13,13 @@ curr = ""
 for sent in sentences:
     curr += sent + " "
     if len(curr) >= max_length:
-        chunks.append(curr)
+        chunks.append(curr + )
         curr = ""
 
-with open("output.txt", "w") as f:
-    for chunk in chunks:
-        f.write(chunk + "\n\n\n\n\n")
+
+with open("output.txt", "w") as f: 
+    for chunk in chunks: 
+        f.write(chunk + "\n\n\n\n\n") # 5 newlines between chunks
 
 print("finished chunking")
 #Summarise every point discussed in the above lecture transcript:
-
